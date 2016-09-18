e3090efaeaebd1282c9b78864861c08d02b3ea31
ever0409 <sajad0409@gmail.com>
2016-09-19 02:08:40 +0430

    Update filter.lua

--- plugins/filter.lua
+++ plugins/filter.lua
@@ -71,7 +71,7 @@ function clear_commandsbad(msg, cmd_name)
 end
 
 local function run(msg, matches)
-  if matches[2] == 'filter' then
+  if matches[2] == 'addword' then
   if not is_momod(msg) then
    return 'only for moderators'
   end
@@ -80,13 +80,13 @@ local function run(msg, matches)
   local text = addword(msg, name)
   return text
   end
-  if matches[2] == 'filterlist' then
+  if matches[2] == 'badwords' then
   return list_variablesbad(msg)
-  elseif matches[2] == 'clean' then
+  elseif matches[2] == 'clearbadwords' then
 if not is_momod(msg) then return '_|_' end
   local asd = '1'
     return clear_commandbad(msg, asd)
-  elseif matches[2] == 'unfilter' or matches[2] == 'rw' then
+  elseif matches[2] == 'remword' or matches[2] == 'rw' then
    if not is_momod(msg) then return '_|_' end
     return clear_commandsbad(msg, matches[3])
   else
@@ -99,13 +99,12 @@ end
 return {
   patterns = {
   "^([!/#])(rw) (.*)$",
-  "^([!/#])(filter) (.*)$",
-   "^([!/#])(unfilter) (.*)$",
-    "^([!/#])(filterlist)$",
-    "^([!/#])(clean) filterlist$",
+  "^([!/#])(addword) (.*)$",
+   "^([!/#])(remword) (.*)$",
+    "^([!/#])(badwords)$",
+    "^([!/#])(clearbadwords)$",
 "^(.+)$",
 	   
   },
   run = run
 }
-
